e2d12e2bac32f0129a46ee03dcb0f14452a0bbec
Vinayak Vispute <vinayakvispute4@gmail.com>
2023-05-19 16:38:20 +0000

    Updated

--- Projects/JavaScript/weatherApp-MiniProject/index.js
+++ Projects/JavaScript/weatherApp-MiniProject/index.js
@@ -6,33 +6,97 @@ const grantUserLocation = document.querySelector(".grant-location-container");
 const searchBarContainer = document.querySelector("[location-searchForm]");
 const loadingScreen = document.querySelector(".loading-screen-container");
 const weatherInfoContainer = document.querySelector(".weather-info-container");
+const cityName = document.querySelector(".cityName")
 
 
 // Variables
 const API_KEY = "c30dafcd337461868e68ebd745919280";
+
+// handling current tab
 let currentTab = userTab;
-currentTab.classList.add("activeTab");
+currentTab.classList.add("currentTab");
 
+// tab Switching concept
+function switchTab(clickedTab) {
+  if (clickedTab != currentTab) {
+    currentTab.classList.remove("currentTab");
+    currentTab = clickedTab;
+    currentTab.classList.add("currentTab");
 
+    if (!searchBarContainer.classList.contains("active")) {
+      grantUserLocation.classList.remove("active");
+      weatherInfoContainer.classList.remove("active");
+      searchBarContainer.classList.add("active");
+    } else {
+      searchBarContainer.classList.remove("active");
+      weatherInfoContainer.classList.remove("active");
+      getfromSessionStorage();
+    }
+  }
+}
 
+async function getfromSessionStorage() {
+  if (navigator.geolocation) {
+    try {
+      console.log("in try")
+      const localCorrdinates = sessionStorage.getItem("userCoordinates");
+      if (!localCorrdinates) {
+      console.log("in if")
+      navigator.geolocation.getCurrentPosition(getLocation);
+      }
+      else {
+      console.log("in else")
 
+        grantUserLocation.classList.add("active");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
+async function getLocation(position) {
+  try {
+    console.log("in getlocation")
 
+    let {latitude,longitude} =position.coords
+    let userCoordinates = { latitude, longitude };
+    sessionStorage.setItem("userCoordinates",JSON.stringify(userCoordinates))
+    // const apiResponse = await fetch(
+    //   `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    // );
+    // console.log("in api fetched")
 
+    // const responseData = await apiResponse.json();
+    // console.log("going to render");
+    // renderWeatherInfo(responseData);
 
-
-
-
-
-
-
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function renderWeatherInfo(data) {
-  let newPara = document.createElement("p");
-  newPara.textContent = `${data?.main?.temp.toFixed(2)}°C`;
+  console.log(data.name);
+  cityName.textContent=data.name;
 
-  document.body.appendChild(newPara);
 }
+
+
+userTab.addEventListener("click", () => {
+  // pass current tab to to function
+  console.log("clicked User");
+  switchTab(userTab);
+});
+
+SearchTab.addEventListener("click", () => {
+  // pass current tab to to function
+  console.log("clicked User");
+
+  switchTab(SearchTab);
+});
+
+
 
 async function fetchWeatherDetails(city) {
   try {
@@ -48,27 +112,4 @@ async function fetchWeatherDetails(city) {
   }
 }
 
-async function getCustomWeatherDetails() {
-  if (navigator.geolocation) {
-    try {
-      navigator.geolocation.getCurrentPosition(getLocation);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
 
-async function getLocation(position) {
-  try {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    const apiResponse = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
-    );
-    const responseData = await apiResponse.json();
-    renderWeatherInfo(responseData);
-    console.log("Wheather Details => ", responseData);
-  } catch (error) {
-    console.log(error);
-  }
-}
