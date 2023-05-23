63b1280b0358d5516fa9d2fcf570b2218e53b7dd
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2023-05-24 03:10:36 +0530

    Updated for useState

--- ReactJs/Day2/src/components/ProductForm.jsx
+++ ReactJs/Day2/src/components/ProductForm.jsx
@@ -1,32 +1,56 @@
-import { useState } from 'react';
-import './ProductForm.css'
+import { useState } from "react";
+import "./ProductForm.css";
 
 function ProductForm() {
+  const [newTitle, setTitle] = useState("");
+  const [newDate, setDate] = useState("");
 
-    const [title, setTitle] = useState('');
-    const [date, setDate] = useState('')
-    function titleChangeHandler(event){
-        console.log(event.target.value)
+  function titleChangeHandler(event) {
+    setTitle(event.target.value);
+    // console.log(event.target.value);
+  }
+  function dateChangeHandler(event) {
+    setDate(event.target.value);
+    // console.log(event.target.value);
+  }
+
+  function submitHandler(event){
+    event.preventDefault();
+
+    const productData = {
+        title:newTitle,
+        date:newDate
     }
+
+    console.log(productData); 
+    setDate('');
+    setTitle('');
     
-    return (
-        <div className="form-container">
-        <form>
-            <div className='new-product_title'>
-                <label>Title</label>
-                <input type='text' onChange={titleChangeHandler}></input>
-            </div>
-            <div  className='new-product_date'>
-                <label>Date</label>
-                <input type='date' min='2023-01-01' max='2023-12-12'></input>
-            </div>
-            <div className='new-product_button'>
-                <button type='submit'>Add Product</button>
-            </div>
-        </form>
+  }
+
+  return (
+    <div className="form-container">
+      <form onSubmit={submitHandler}>
+        <div className="new-product_title">
+          <label>Title</label>
+          <input type="text" value={newTitle} onChange={titleChangeHandler}></input>
         </div>
-    )
+        <div className="new-product_date">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2023-01-01"
+            max="2023-12-12"
+            onChange={dateChangeHandler}
+            value={newDate}
+          ></input>
+        </div>
+        <div className="new-product_button">
+          <button type="submit">Add Product</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-
-export default ProductForm; 
+export default ProductForm;
