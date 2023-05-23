e9a3c40af6542cb67e725b8c81045fe200c6b726
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2023-05-23 21:53:43 +0530

    Day2 Created

---
+++ ReactJs/Day2/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className="text-3xl text-orange-400 font-bold underline">
+        Hello world!
+      </h1>  
+    </>
+  )
+}
+
+export default App
