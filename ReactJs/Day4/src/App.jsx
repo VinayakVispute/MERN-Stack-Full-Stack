9df1c833ec1f43c85c9c712de69c419b21dfa657
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2023-05-26 02:53:08 +0530

    day4

--- ReactJs/Day4/src/App.jsx
+++ ReactJs/Day4/src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  
+  const [text, setText] = useState("");
 
+  useEffect(() => {
+    console.log("UI is Rendering");
+  }, []);
+
+  function changeHandler(event) {
+    setText(event.target.value);
+    console.log(text);
+  }
   return (
-    <>
-      
-      <h1 className="text-3xl text-orange-400 font-bold underline">
-        Hello world!
-      </h1>  
-    </>
-  )
+    <div className="w-screen h-screen flex justify-center items-center">
+      <input type="text" onChange={changeHandler} />
+    </div>
+  );
 }
 
-export default App
+export default App;
